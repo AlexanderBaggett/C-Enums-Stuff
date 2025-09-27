988c8824de356dcbb3f694a00274a9a2214f1ef2
AlexanderBaggett <alexander.baggett@gmail.com>
2025-09-27 14:09:58 -0500

    updated project

---
+++ EnumUtils/EnumUtils.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Enums
+{
+    public static class EnumExtensions
+    {
+        //Break the enum apart based on changes in case.
+        public static string ToText(this Enum value)
+        {
+            string input = value.ToString();
+            string output = Regex.Replace(input, "([a-z])([A-Z])", "$1 $2");
+            return output;
+        }
+
+        public static int ToInt(this Enum value)
+        {
+            return Convert.ToInt32(value);
+        }
+        public static long ToLong(this Enum value)
+        {
+            return Convert.ToInt64(value);
+        }
+
+        public static List<int> GetAllPossibleValues<T>() where T : struct, Enum
+        {
+            var enumType = typeof(T);
+            var isFlagsEnum = enumType.IsDefined(typeof(FlagsAttribute), false);
+
+            var values = Enum.GetValues<T>().Select(x => x.ToInt()).ToList();
+            if (!isFlagsEnum)
+            {
+                return values;
+            }
+
+            //combine all possible values (bitwise)
+            var sum = values.Aggregate(0, (acc, val) => acc | val);
+
+            values.Clear();
+            for (int i = 0; i < sum; i++)
+            {
+                if ((i & sum) == i)
+                {
+                    values.Add(i);
+                }
+            }
+            return values;
+        }
+
+        private static List<int> GetFlagEnumPossibleValues<T>() where T : struct, Enum
+        {
+            var values = Enum.GetValues<T>().Select(x => x.ToInt()).ToList();
+            var sum = values.Aggregate(0, (acc, val) => acc | val);
+
+            values.Clear();
+            for (int i = 0; i < sum; i++)
+            {
+                if ((i & sum) == i)
+                {
+                    values.Add(i);
+                }
+            }
+            return values;
+        }
+
+        public static T ToEnum<T>(this int value) where T : struct, Enum
+        {
+            var enumType = typeof(T);
+            var isFlagsEnum = enumType.IsDefined(typeof(FlagsAttribute), false);
+
+            if (!isFlagsEnum)
+            {
+                if (!Enum.IsDefined(enumType, value))
+                {
+                    throw new ArgumentException($"Value {value} is not defined in enum {enumType.Name}");
+                }
+                return (T)Enum.Parse(enumType, value.ToString());
+            }
+            else
+            {
+                if (!GetFlagEnumPossibleValues<T>().Contains(value))
+                {
+                    throw new ArgumentException($"Value {value} is not a valid combination in enum {enumType.Name}");
+                }
+                return (T)Enum.Parse(enumType, value.ToString());
+            }
+        }
+
+        public static string ToText<T>(this int value) where T : struct, Enum
+        {
+            var enumType = typeof(T);
+            var isFlagsEnum = enumType.IsDefined(typeof(FlagsAttribute), false);
+
+            if (!isFlagsEnum)
+            {
+                if (!Enum.IsDefined(enumType, value))
+                {
+                    throw new ArgumentException($"Value {value} is not defined in enum {enumType.Name}");
+                }
+            }
+            else
+            {
+                if (!GetFlagEnumPossibleValues<T>().Contains(value))
+                {
+                    throw new ArgumentException($"Value {value} is not a valid combination in enum {enumType.Name}");
+                }
+            }
+
+            var enumeration = (T)Enum.Parse(enumType, value.ToString());
+            string input = enumeration.ToString();
+            string output = Regex.Replace(input, "([a-z])([A-Z])", "$1 $2");
+            return output;
+        }
+
+        public static List<string> ToTextList<T>() where T : struct, Enum
+        {
+            return Enum.GetValues<T>().Select(x => x.ToText()).ToList();
+        }
+        public static List<string> ToTextList<Type>(this Type t) where Type : struct, Enum
+        {
+            return Enum.GetValues<Type>().Select(x => x.ToText()).ToList();
+        }
+    }
+}
---
+++ TestProject1/EnumUtils_Tests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace TestProject1
+{
+    using Enums;
+    using Shouldly;
+    enum TicketStatus
+    {
+        PendingInvestigation = 0,
+        Assigned,
+        Active,
+        WorkComplete,
+        FailedInspection,
+        RejectedByClient,
+        Cancelled
+    }
+
+    [Flags]
+    enum PlayerFlags
+    {
+        Admin = 0,
+        Invulerable = 1,
+        NoClip = 2,
+        Flying = 4,
+        Invisible = 8,
+        Frozen = 32,
+    }
+    public class EnumUtils_Tests
+    {
+        [Fact]
+        public void ToText_Breaks_Apart_Words()
+        {
+            var status = TicketStatus.PendingInvestigation;
+            status.ToText().ShouldBe("Pending Investigation");
+        }
+
+        [Fact]
+        public void ToEnum_Generates_EnumValues()
+        {
+            var status = 0;
+            status.ToEnum<TicketStatus>().ShouldBe(TicketStatus.PendingInvestigation);
+        }
+
+        [Fact]
+        public void ToTextList_Breaks_ApartWords()
+        {
+            var textList = EnumExtensions.ToTextList(TicketStatus.PendingInvestigation);
+            textList.ShouldContain("Pending Investigation");
+            textList.ShouldContain("Failed Inspection");
+            textList.ShouldContain("Rejected By Client");
+        }
+        [Fact]
+        public void Flag_Enums_Produce_Correct_Results()
+        {
+            var flags = 3.ToEnum<PlayerFlags>();
+            flags.ToText().ShouldBe("Invulerable, No Clip");
+        }
+
+        [Fact]
+        public void Can_Detect_Flags_attribute()
+        {
+            var enumType = typeof(PlayerFlags);
+            var detected = enumType.IsDefined(typeof(FlagsAttribute), false);
+            detected.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void ToEnum_Can_Detect_Bad_Integer_Values()
+        {
+            var status = 12;
+            Should.Throw<ArgumentException>(() => status.ToEnum<TicketStatus>());
+        }
+
+        [Fact]
+        public void ToEnum_Can_Detect_BadCombinations()
+        {
+            var playerFlag = 31;
+            Should.Throw<ArgumentException>(() => playerFlag.ToEnum<PlayerFlags>());
+        }
+
+
+        [Fact]
+        public void ToText_Can_Detect_Bad_Integer_Values()
+        {
+            var status = 12;
+            Should.Throw<ArgumentException>(() => status.ToText<TicketStatus>());
+        }
+
+        [Fact]
+        public void ToText_Can_Detect_BadCombinations()
+        {
+            var playerFlag = 31;
+            Should.Throw<ArgumentException>(() => playerFlag.ToText<PlayerFlags>());
+        }
+    }
+}
